af6c0501685482f38578eb4156766903a6717241
Michele Pesante <pesante.michele.96@gmail.com>
2018-01-27 00:34:23 +0100

    Aggiunto script PlayerController.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public KeyCode hackerButton;
+	public KeyCode jumpButton;
+	public KeyCode speedButton;
+	public KeyCode strengthButton;
+	public KeyCode triggerButton;
+
+	public GameObject gc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.name == "Player1") {
+			if (Input.GetKeyDown(jumpButton) && Input.GetKeyDown(triggerButton)) {
+				// SendAb ("Jump", 2);
+			}
+		}
+	}
+}
