965bf306da65aa4bc0132e903c876f4c6341236e
Michele Pesante <pesante.michele.96@gmail.com>
2018-01-26 21:50:12 +0100

    Aggiunto uno SceneManager.

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+
+}
