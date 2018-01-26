6a43849280c3a5d32d1052a9d3d1527cb9c329ef
Michele Pesante <pesante.michele.96@gmail.com>
2018-01-26 20:13:24 +0100

    Creazione progetto con camera split screen

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	/// <summary>
+	/// Riferimento al personaggio.
+	/// </summary>
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		// Se è la telecamera 1.
+		if (this.name == "Main Camera")
+			// Si attacca al player 1.
+			player = GameObject.Find ("Player1");
+		// Se è la telecamera 2.
+		else if (this.name == "Main Camera 2")
+			// Si attacca al player 2.
+			player = GameObject.Find ("Player2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// La mia posizione diventa quella del player.
+		transform.position = player.transform.position;
+		// La mia rotazione diventa quella del player.
+		transform.rotation = player.transform.rotation;
+	}
+}
