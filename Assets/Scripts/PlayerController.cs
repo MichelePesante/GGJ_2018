047349ec692a7067f250e48e6e359fa7de82200a
Michele Pesante <pesante.michele.96@gmail.com>
2018-01-27 00:44:02 +0100

    Aggiornato script player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,18 +10,54 @@ public class PlayerController : MonoBehaviour {
 	public KeyCode strengthButton;
 	public KeyCode triggerButton;
 
-	public GameObject gc;
+	public Abilities playerAbilities;
 
 	// Use this for initialization
 	void Start () {
-		
+		playerAbilities = GameObject.FindObjectOfType<Abilities> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (this.gameObject.name == "Player1") {
-			if (Input.GetKeyDown(jumpButton) && Input.GetKeyDown(triggerButton)) {
-				// SendAb ("Jump", 2);
+			if (Input.GetKey(hackerButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Hacker", 2);
+			}
+		}
+		if (this.gameObject.name == "Player1") {
+			if (Input.GetKey(jumpButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Jump", 2);
+			}
+		}
+		if (this.gameObject.name == "Player1") {
+			if (Input.GetKey(speedButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Speed", 2);
+			}
+		}
+		if (this.gameObject.name == "Player1") {
+			if (Input.GetKey(strengthButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Strength", 2);
+			}
+		}
+
+		if (this.gameObject.name == "Player2") {
+			if (Input.GetKey(hackerButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Hacker", 1);
+			}
+		}
+		if (this.gameObject.name == "Player2") {
+			if (Input.GetKey(jumpButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Jump", 1);
+			}
+		}
+		if (this.gameObject.name == "Player2") {
+			if (Input.GetKey(speedButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Speed", 1);
+			}
+		}
+		if (this.gameObject.name == "Player2") {
+			if (Input.GetKey(strengthButton) && Input.GetKey(triggerButton)) {
+				playerAbilities.SendAb ("Strength", 1);
 			}
 		}
 	}
