29ab1ad8d876c9315b559ae982f85727f8a26969
Pigna15 <giacomo@MacBook-Pro-di-Pignoni.local>
2018-01-26 20:35:31 +0100

    Aggiunto Script Player

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour {
+
+	public float Speed = 0.1f;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += new Vector3 (0, 0, Speed);
+		} 
+		else if (Input.GetKey (KeyCode.A)) {
+			transform.position += new Vector3 (-Speed, 0, 0);
+		} 
+		else if (Input.GetKey (KeyCode.D)) {
+			transform.position += new Vector3 (Speed, 0, 0);
+		} 
+		else if (Input.GetKey (KeyCode.S)) {
+			transform.position += new Vector3 (0, 0, -Speed);
+		}
+	}
+}
