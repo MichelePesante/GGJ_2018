1aaf2124e65501e72cb189e207348e2765222fbf
Michele Pesante <pesante.michele.96@gmail.com>
2018-01-27 01:15:45 +0100

    Aggiunto funzionalità pulsante

---
+++ Assets/Scripts/HackerButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackerButton : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player")
+			rb.AddForce(Vector3.forward * 0.1f);
+	}
+}
